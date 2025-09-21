1c9221e5bba13efa8ae59a23394fb4f8d856ee22
Apex <kunalgulve209@gmail.com>
2025-09-22 01:23:43 +0530

    Product of array Itself

---
+++ ProductOfArrayExceptItself.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> bruteForce(vector<int>& nums){
+    int n = nums.size();
+    vector<int> ans (n,1);
+
+    for(int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++)
+        {
+            if (i != j)
+            {
+                ans[i] *= nums[j];
+                
+            }
+            
+        }
+    
+
+    }
+    return ans;
+}
+
+vector<int> Optimize(vector<int>& nums){
+    int n = nums.size();
+    vector<int> ans(n,1);
+    vector<int> prefix(n,1);
+    vector<int> suffix(n,1);
+
+    for(int i = 1; i < n; i++){
+        prefix[i] = prefix[i - 1] * nums[i - 1];
+    }
+    for (int i = n - 2; i >= 0; i--)
+    {
+        suffix[i] = suffix [i + 1] * nums[i + 1];
+
+    }
+    for (int i = 0; i < n; i++)
+    {
+        ans[i] = prefix[i] * suffix[i];
+    }
+  return ans;
+}
+  
+vector<int> SpaceComplexity(vector<int>& nums){
+    int n = nums.size();
+    vector<int> ans(n,1);
+
+    for(int i = 1; i < n; i++){
+        ans[i] = ans[i - 1] * nums[i - 1];
+    }
+    int suffix = 1;
+    for (int i = n - 2; i >= 0; i--)
+    {
+        suffix *= nums[ i + 1];
+        ans[i] *= suffix;
+    }
+    return ans;
+
+  }
+    
+int main(){
+    vector<int> nums = {1, 2, 3, 4};
+    auto res1 = bruteForce(nums);
+    auto res2 = Optimize(nums);
+    auto res3 = SpaceComplexity(nums);
+    for (int x : res1) cout  << x << " ";
+    cout << endl;
+    for (int x :  res2) cout << x << " ";
+    cout << endl;
+  for (int x : res3 ) cout << x << " ";
+  cout << endl;
+    return 0;
+}
